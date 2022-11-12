5ec918a76b4eb54226f77f023e26c0f41038bdd3
TypeErrorRic <ricardo.pabon@correounivalle.edu.co>
2022-11-12 16:26:43 -0500

    Guardar cambios

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,4 @@
-const { Router }  = require('express');
+const { Router, application }  = require('express');
 
 const router = Router();
 
@@ -6,8 +6,24 @@ router.get('/dashboard', (req, res) => {
   res.render('dashboard'); 
 });
 
-router.get('about', (req, res) => {
-  res.render('index', {title: "Mi primera pagina con Ejs."});
+router.use((req,res, next) => {
+  next();
+})
+
+router.get('/about', (req, res) => {
+  const users = [
+    {
+      name: "Ricardo",
+      lastname: "Pabon",
+      id: 1,
+    },
+    {
+      name: "Meriyein",
+      lastname: "Garcia",
+      id: 2,
+    }
+  ]
+  res.render('index', {title: "Mi primera pagina con Ejs.", users});
 })
 
 router.get('/', (req, res) => {
